be7470ba13778d57c7cd08221f0ddb3582a1e26c
Mohammad Rahmani <rto1680@gmail.com>
2022-03-25 22:54:28 +0430

    Add api service class

---
+++ lib/services/networking/ApiService.dart
@@ -0,0 +1,108 @@
+import 'package:day59/services/networking/BaseProvider.dart';
+
+class ApiService {
+  final BaseProvider _baseProvider;
+
+  ApiService(this._baseProvider);
+
+  Future<JSON> get<T>({
+    required String endpoint,
+    JSON? query,
+    Map<String, String>? headers,
+    bool requiresAuthToken = false,
+  }) async {
+    var customHeaders = {
+      'Accept': 'application/json',
+      requiresAuthToken ? 'Authorization': '' : '',
+    };
+
+    if (headers != null) {
+      customHeaders.addAll(headers);
+    }
+
+    final response = await _baseProvider.get(
+      endpoint,
+      headers: customHeaders,
+      query: query
+    );
+
+    return response.body;
+  }
+
+  Future<JSON> post<T>({
+    required String endpoint,
+    JSON? body,
+    JSON? query,
+    Map<String, String>? headers,
+    bool requiresAuthToken = false,
+  }) async {
+    var customHeaders = {
+      'Accept': 'application/json',
+      requiresAuthToken ? 'Authorization': '' : '',
+    };
+
+    if (headers != null) {
+      customHeaders.addAll(headers);
+    }
+
+    final response = await _baseProvider.post(
+      endpoint,
+      body,
+      headers: customHeaders,
+      query: query
+    );
+
+    return response.body;
+  }
+
+  Future<JSON> put<T>({
+    required String endpoint,
+    JSON? body,
+    JSON? query,
+    Map<String, String>? headers,
+    bool requiresAuthToken = false,
+  }) async {
+    var customHeaders = {
+      'Accept': 'application/json',
+      requiresAuthToken ? 'Authorization': '' : '',
+    };
+
+    if (headers != null) {
+      customHeaders.addAll(headers);
+    }
+
+    final response = await _baseProvider.put(
+      endpoint,
+      body,
+      headers: customHeaders,
+      query: query
+    );
+
+    return response.body;
+  }
+  
+  Future<JSON> delete<T>({
+    required String endpoint,
+    JSON? body,
+    JSON? query,
+    Map<String, String>? headers,
+    bool requiresAuthToken = false,
+  }) async {
+    var customHeaders = {
+      'Accept': 'application/json',
+      requiresAuthToken ? 'Authorization': '' : '',
+    };
+
+    if (headers != null) {
+      customHeaders.addAll(headers);
+    }
+
+    final response = await _baseProvider.delete(
+      endpoint,
+      headers: customHeaders,
+      query: query
+    );
+
+    return response.body;
+  }
+}
