1d83a62f8f00d6cf747ab44f392535bad80708ee
Mohammad Rahmani <rto1680@gmail.com>
2022-03-30 21:12:26 +0430

    Add category model

---
+++ lib/models/categories/CategoryModel.dart
@@ -0,0 +1,15 @@
+import 'package:day59/shared/typedef.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'CategoryModel.freezed.dart';
+part 'CategoryModel.g.dart';
+
+@freezed
+class CategoryModel with _$CategoryModel {
+  factory CategoryModel({
+    required int id,
+    required String name
+  }) = _CategoryModel;
+
+  factory CategoryModel.fromJson(JSON json) => _$CategoryModelFromJson(json);
+}
