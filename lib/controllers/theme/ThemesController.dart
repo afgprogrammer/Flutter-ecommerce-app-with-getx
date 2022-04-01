ab29bf11624541b7e73e073862a84365a669d384
Mohammad Rahmani <rto1680@gmail.com>
2022-04-01 23:03:35 +0430

    Add theme controller

---
+++ lib/controllers/theme/ThemesController.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class ThemesController extends GetxController {
+  final storage = GetStorage();
+
+  var theme = 'light';
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    getThemeState();
+  }
+
+  getThemeState() {
+    if (storage.read('theme') != null) {
+      return changeTheme(storage.read('theme'));
+    } 
+    
+    changeTheme('light');
+  }
+
+  void changeTheme(String value) {
+    theme = value;
+    storage.write('theme', value);
+
+    if (value == 'system') Get.changeThemeMode(ThemeMode.system);
+    if (value == 'light') Get.changeThemeMode(ThemeMode.light);
+    if (value == 'dark') Get.changeThemeMode(ThemeMode.dark);
+
+    update();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
+import 'package:day59/controllers/theme/ThemesController.dart';
 import 'package:day59/themes/Themes.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:day59/AppBinding.dart';
 import 'package:day59/routes/routes.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+
   runApp(App());
 }
 class App extends StatelessWidget {
+  final ThemesController themeController = Get.put(ThemesController());
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +21,27 @@ class App extends StatelessWidget {
       title: 'Flutter e-commerce app',
       theme: Themes.lightTheme,
       darkTheme: Themes.darkTheme,
+      themeMode: getThemeMode(themeController.theme),
       getPages: Routes.routes,
       initialRoute: Routes.INITIAL,
       initialBinding: AppBinding(),
     );
+  }
+
+  ThemeMode getThemeMode(String type) {
+    ThemeMode themeMode = ThemeMode.system;
+    switch (type) {
+      case "system":
+        themeMode = ThemeMode.system;
+        break;
+      case "dark":
+        themeMode = ThemeMode.dark;
+        break;
+      default:
+        themeMode = ThemeMode.light;
+        break;
+    }
+
+    return themeMode;
   }
 }
