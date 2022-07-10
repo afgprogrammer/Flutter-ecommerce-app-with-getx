46be7aaca33a626f9b06b0896b4cc3f248022a55
Mohammad Rahmani <rto1680@gmail.com>
2022-07-10 21:25:28 +0430

    Add product page route

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,9 @@
 import 'package:day59/controllers/home/HomeBinding.dart';
+import 'package:day59/controllers/products/ProductBinding.dart';
 import 'package:day59/views/authentication/LoginPage.dart';
 import 'package:day59/views/authentication/SplashPage.dart';
 import 'package:day59/views/home/HomePage.dart';
+import 'package:day59/views/product/ProductPage.dart';
 import 'package:get/route_manager.dart';
 
 class Routes {
@@ -20,6 +22,11 @@ class Routes {
       name: '/home', 
       page: () => HomePage(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: '/product/:id', 
+      page: () => ProductPage(),
+      binding: ProductBinding(),
     )
   ];
 }
